fac2a5d191fcf499cb6708d35767f96b747c8e8e
Darkphoenix98 <pablomcjq@gmail.com>
2024-04-18 10:54:32 -0500

    BASEURL

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4001/'
+    //baseURL: 'http://localhost:4001/'
+    baseURL: 'https://movies-lm2x.onrender.com/'
 });
 
 export {
