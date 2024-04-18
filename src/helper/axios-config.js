ad5a52e906cef457564363d53ba9e0bee06a838d
Darkphoenix98 <pablomcjq@gmail.com>
2024-04-18 11:23:05 -0500

    CCC

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     
-    baseURL: 'https://movies-5eef.onrender.com/'
+    baseURL:'https://movies-5eef.onrender.com'
 });
 
 export {
